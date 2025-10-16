3953e336b2c49ebff2c60aa584bbcd57e560deab
emmvs <emma.ruenzel@1komma5grad.com>
2025-10-16 12:38:58 +0200

    Add new task for jerik

---
+++ string_manipulation.rb
@@ -0,0 +1,37 @@
+# Warning:
+# - One line of code for each method
+# - Just look in the doc for the right method of the String, Integer, and Array classes!
+
+def get_rid_of_surrounding_whitespaces(a_string)
+  # TODO
+end
+
+def belongs_to?(a_string, a_word)
+  # TODO
+end
+
+def replace(initial_string, old_letter, new_letter)
+  # TODO
+end
+
+def exactly_divide(an_integer, a_divider)
+  # TODO: return division of an_integer by a_divider (computing the floating division, NOT the euclidean one)
+  # example: exactly_divide(13, 4) => 3.25
+end
+
+def divisible_by_two?(an_integer)
+  # TODO
+end
+
+def random_subset(an_array, sample_size)
+  # TODO: return a random subset of an_array with size sample_size
+  # example: random_subset(("a".."z").to_a, 4) => ["u", "q", "l", "t"]
+end
+
+def randomize(an_array)
+  # TODO
+end
+
+def ascending_order(an_array)
+  # TODO
+end
